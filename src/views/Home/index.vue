dbd1d217625ee8eaadfddb1076cd38273cb60480
jarrymeng <jarrymeng@aliyun.com>
2019-07-01 11:55:51 +0800

    change

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -17,6 +17,54 @@
       </el-card>
     </el-col>
   </el-row>
+  <el-row>
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
+  </el-row>
+
+  <el-row>
+    <el-button plain>朴素按钮</el-button>
+    <el-button type="primary" plain>主要按钮</el-button>
+    <el-button type="success" plain>成功按钮</el-button>
+    <el-button type="info" plain>信息按钮</el-button>
+    <el-button type="warning" plain>警告按钮</el-button>
+    <el-button type="danger" plain>危险按钮</el-button>
+  </el-row>
+
+  <el-row>
+    <el-button round>圆角按钮</el-button>
+    <el-button type="primary" round>主要按钮</el-button>
+    <el-button type="success" round>成功按钮</el-button>
+    <el-button type="info" round>信息按钮</el-button>
+    <el-button type="warning" round>警告按钮</el-button>
+    <el-button type="danger" round>危险按钮</el-button>
+  </el-row>
+
+  <el-row>
+    <el-button icon="el-icon-search" circle></el-button>
+    <el-button type="primary" icon="el-icon-edit" circle></el-button>
+    <el-button type="success" icon="el-icon-check" circle></el-button>
+    <el-button type="info" icon="el-icon-message" circle></el-button>
+    <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+    <el-button type="danger" icon="el-icon-delete" circle></el-button>
+  </el-row>
+  <el-row :gutter="12">
+    <el-carousel indicator-position="outside">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3>{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+  </el-row>
+  <el-row>
+    <el-progress :text-inside="true" :stroke-width="26" :percentage="70"></el-progress>
+    <el-progress :text-inside="true" :stroke-width="24" :percentage="100" status="success"></el-progress>
+    <el-progress :text-inside="true" :stroke-width="22" :percentage="80" status="warning"></el-progress>
+    <el-progress :text-inside="true" :stroke-width="20" :percentage="50" status="exception"></el-progress>
+  </el-row>
 </div>
 </template>
 
@@ -25,6 +73,29 @@ export default {
   name: 'home',
   components: {
 
+  },
+  mounted(){
+    
+  },
+  methods: {
+
   }
 }
 </script>
+<style lang="scss">
+.el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+}
+</style>
